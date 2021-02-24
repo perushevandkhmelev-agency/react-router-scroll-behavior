d2e7d5ef45bb547415db5dec9055928bd63edc35
Maxim Postnikov <itsmaximpostnikov@gmail.com>
2021-02-24 16:35:18 +0300

    Update import PropTypes

--- src/RouterScrollContext.js
+++ src/RouterScrollContext.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import RouterContext from 'react-router/lib/RouterContext'
 import standard from './behaviors/standard'
 
